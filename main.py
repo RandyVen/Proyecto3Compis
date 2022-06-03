8ea46b20daf9381b488cda3d24138908a17ef7a9
RandyVen <35413122+RandyVen@users.noreply.github.com>
2022-06-02 19:38:15 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+from Parserdos import analyzedos
+from scanner import scan
+from generate_scanner import create
+from parsed_productions import analyze_productions
+import os, sys, subprocess
+
+def open_file(filename):
+    if sys.platform == "win32":
+        os.startfile(filename)
+    else:
+        opener = "open" if sys.platform == "darwin" else "xdg-open"
+        subprocess.call([opener, filename])
+
+def main():
+    print("archivo?")
+    archivo = 'HexNumber.ATG'
+    input_file = open("./input/" + archivo)
+    data = input_file.read()
+    input_file.close()
+    name, characters, keywords, tokens, productions = scan(data)
+    code = analyze_productions(productions, tokens, keywords)
+    final_dfa, dfas = analyzedos(name, characters, keywords, tokens)
+    open_file('Stat.pdf')
+    open_file('Factor.pdf')
+    open_file('term.pdf')
+    open_file('expression.pdf')
+    open_file('expression.pdf')
+    create(final_dfa, dfas, name, code)
+    
+if __name__ == "__main__":
+    main()
